cadccd80f3de2942449be52894ad59baa65cdf14
Serkan Başdağ <40989158+serkanbasdag@users.noreply.github.com>
2019-03-27 09:31:57 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+
+eventListeners();
+
+
+function eventListeners(){
+    document.getElementById("translate-form").addEventListener("submit",translateWord);
+    document.getElementById("language").onchange = function(){
+        //arayüz işlemleri.
+
+        ui.changeUI();
+    }
+}
+const translate = new Translate(document.getElementById("word").value,document.getElementById("language").value);
+const ui = new UI();
+
+function translateWord(e){
+    translate.changeParameters(document.getElementById("word").value,document.getElementById("language").value);
+
+    translate.translateWord(function(response){
+        if(response)
+        {
+            ui.displayTranslate(response);
+        }
+        else{
+            
+           // console.log(response);
+        }
+    });
+    e.preventDefault();
+}
